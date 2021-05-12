8575b4b5d93d3a2f3cdc69acafb49e1a1419d545
oNevion <onevion@gmail.com>
2021-05-12 13:10:37 -0400

    Update CWX charts with new fact sheet changes

--- assets/ChartBuilder/public/scripts/cwx.js
+++ assets/ChartBuilder/public/scripts/cwx.js
@@ -15,7 +15,7 @@ body {
 
     #chartdiv {
         width:1080px!important;
-		height:460px!important;
+		height:560px!important;
     }
 
         #chartdiv2 {
@@ -68,24 +68,31 @@ var dateAxis = chart1.xAxes.push(new am4charts.DateAxis());
 dateAxis.renderer.labels.template.fontWeight = "Bold";
 dateAxis.renderer.labels.template.fontSize = "20px";
 dateAxis.renderer.grid.template.disabled = true;
-dateAxis.renderer.minGridDistance = 50;
+dateAxis.renderer.minGridDistance = 30;
 //dateAxis.renderer.labels.template.dx = 10;
 dateAxis.renderer.minLabelPosition = -.04;
 dateAxis.extraMax = 0.03; 
 dateAxis.startLocation = 0.5;
 dateAxis.endLocation = 0.4;
 dateAxis.marginTop = 10;
+dateAxis.dateFormats.setKey("month", "MM/yyyy");
+dateAxis.periodChangeDateFormats.setKey("month", "MM/yyyy");
+dateAxis.renderer.labels.template.rotation = -45;
+dateAxis.renderer.labels.template.verticalCenter = "middle";
+dateAxis.renderer.labels.template.horizontalCenter = "middle";
+
 
 var valueAxis = chart1.yAxes.push(new am4charts.ValueAxis());
 valueAxis.renderer.labels.template.fontWeight = "Bold";
 valueAxis.renderer.labels.template.fontSize = "20px";
+valueAxis.extraMin = 0.03; 
 
 valueAxis.numberFormatter = new am4core.NumberFormatter();
 valueAxis.numberFormatter.numberFormat = "'$'#,###";
 
 // Create series
 var series1 = chart1.series.push(new am4charts.LineSeries());
-series1.dataFields.valueY = "CWXAX";
+series1.dataFields.valueY = "Portfolio";
 series1.dataFields.dateX = "Date";
 series1.name = "CWXAX";
 series1.strokeWidth = 3;
@@ -94,7 +101,7 @@ series1.tensionX = 0.9;
 series1.stroke = am4core.color("#08da94");
 
 var series2 = chart1.series.push(new am4charts.LineSeries());
-series2.dataFields.valueY = "S&P 500 TR Index";
+series2.dataFields.valueY = "Index";
 series2.dataFields.dateX = "Date";
 series2.name = "S&P 500 TR Index";
 series2.strokeWidth = 3;
@@ -120,84 +127,84 @@ chart1.exporting.menu.items = [{
 chart1.exporting.filePrefix = "CWX_10k";
 
 
-// ################################ CHART 2 CWX - Annual Return (Bar) ################################################
+// // ################################ CHART 2 CWX - Annual Return (Bar) ################################################
 
-// Themes begin
-am4core.useTheme(am4themes_amcharts);
-// Themes end
+// // Themes begin
+// am4core.useTheme(am4themes_amcharts);
+// // Themes end
 
-// Create chart instance
-var chart2 = am4core.create("chartdiv2", am4charts.XYChart);
+// // Create chart instance
+// var chart2 = am4core.create("chartdiv2", am4charts.XYChart);
 
-// Add percent sign to all numbers
-chart2.numberFormatter.numberFormat = "#.#'%'";
+// // Add percent sign to all numbers
+// chart2.numberFormatter.numberFormat = "#.#'%'";
 
-// Set up data source
-chart2.dataSource.url = "../Data/Backups/Catalyst/CWX/CWX_EXPORT_AnnualReturns.csv";
-chart2.dataSource.parser = new am4core.CSVParser();
-chart2.dataSource.parser.options.useColumnNames = true;
+// // Set up data source
+// chart2.dataSource.url = "../Data/Backups/Catalyst/CWX/CWX_EXPORT_AnnualReturns.csv";
+// chart2.dataSource.parser = new am4core.CSVParser();
+// chart2.dataSource.parser.options.useColumnNames = true;
 
-// Create axes
-var categoryAxis = chart2.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "Year";
-categoryAxis.renderer.labels.template.fontSize = "23px";
-categoryAxis.renderer.labels.template.fontWeight = "Bold";
-categoryAxis.renderer.grid.template.disabled = false;
-categoryAxis.renderer.grid.template.location = 0;
-categoryAxis.renderer.minGridDistance = 30;
-categoryAxis.renderer.cellStartLocation = 0.1;
-categoryAxis.renderer.cellEndLocation = 0.9;
-categoryAxis.renderer.grid.template.strokeOpacity = .2;
-categoryAxis.renderer.labels.template.textAlign = "middle";
+// // Create axes
+// var categoryAxis = chart2.xAxes.push(new am4charts.CategoryAxis());
+// categoryAxis.dataFields.category = "Year";
+// categoryAxis.renderer.labels.template.fontSize = "23px";
+// categoryAxis.renderer.labels.template.fontWeight = "Bold";
+// categoryAxis.renderer.grid.template.disabled = false;
+// categoryAxis.renderer.grid.template.location = 0;
+// categoryAxis.renderer.minGridDistance = 30;
+// categoryAxis.renderer.cellStartLocation = 0.1;
+// categoryAxis.renderer.cellEndLocation = 0.9;
+// categoryAxis.renderer.grid.template.strokeOpacity = .2;
+// categoryAxis.renderer.labels.template.textAlign = "middle";
 
-var label = categoryAxis.renderer.labels.template;
-label.wrap = true;
-label.maxWidth = 100;
+// var label = categoryAxis.renderer.labels.template;
+// label.wrap = true;
+// label.maxWidth = 100;
 
-var valueAxis = chart2.yAxes.push(new am4charts.ValueAxis());
-valueAxis.renderer.grid.template.disabled = true;
-valueAxis.renderer.labels.template.fontWeight = "Bold";
-valueAxis.renderer.labels.template.fontSize = "23px";
+// var valueAxis = chart2.yAxes.push(new am4charts.ValueAxis());
+// valueAxis.renderer.grid.template.disabled = true;
+// valueAxis.renderer.labels.template.fontWeight = "Bold";
+// valueAxis.renderer.labels.template.fontSize = "23px";
 
-// Create series
-var series = chart2.series.push(new am4charts.ColumnSeries());
-series.dataFields.valueY = "CWXAX";
-series.dataFields.categoryX = "Year";
-series.name = "CWXAX";
-series.clustered = true;
-series.fill = am4core.color("#08da94");
-series.strokeWidth = 0;
-series.columns.template.width = am4core.percent(100);
-
-
-
-
-var series2 = chart2.series.push(new am4charts.ColumnSeries());
-series2.dataFields.valueY = "S&P 500 TR";
-series2.dataFields.categoryX = "Year";
-series2.name = "S&P 500 TR";
-series2.clustered = true;
-series2.fill = am4core.color("#2d7abf");
-series2.strokeWidth = 0;
-series2.columns.template.width = am4core.percent(100);
+// // Create series
+// var series = chart2.series.push(new am4charts.ColumnSeries());
+// series.dataFields.valueY = "CWXAX";
+// series.dataFields.categoryX = "Year";
+// series.name = "CWXAX";
+// series.clustered = true;
+// series.fill = am4core.color("#08da94");
+// series.strokeWidth = 0;
+// series.columns.template.width = am4core.percent(100);
 
 
 
-// Add legend
-chart2.legend = new am4charts.Legend();
-chart2.legend.labels.template.fontSize = "23px";
-chart2.legend.labels.template.truncate = false;
+
+// var series2 = chart2.series.push(new am4charts.ColumnSeries());
+// series2.dataFields.valueY = "S&P 500 TR";
+// series2.dataFields.categoryX = "Year";
+// series2.name = "S&P 500 TR";
+// series2.clustered = true;
+// series2.fill = am4core.color("#2d7abf");
+// series2.strokeWidth = 0;
+// series2.columns.template.width = am4core.percent(100);
 
 
-// Export this stuff
-chart2.exporting.menu = new am4core.ExportMenu();
-chart2.exporting.menu.items = [{
-  "label": "...",
-  "menu": [
-          { "type": "svg", "label": "SVG" },
-  ]
-}];
-chart2.exporting.filePrefix = "CWX_AnnualReturn";
+
+// // Add legend
+// chart2.legend = new am4charts.Legend();
+// chart2.legend.labels.template.fontSize = "23px";
+// chart2.legend.labels.template.truncate = false;
+
+
+// // Export this stuff
+// chart2.exporting.menu = new am4core.ExportMenu();
+// chart2.exporting.menu.items = [{
+//   "label": "...",
+//   "menu": [
+//           { "type": "svg", "label": "SVG" },
+//   ]
+// }];
+// chart2.exporting.filePrefix = "CWX_AnnualReturn";
 
 // ################################ CHART 3 CWX Institutional 10k Chart (Line) ################################################
 
@@ -381,7 +388,6 @@ chart4.exporting.filePrefix = "CWX_Brochure_2020BearMarket";
 
 function loadFrame() {
      chart1.exporting.export('svg');
-     chart2.exporting.export('svg');
      chart3.exporting.export('svg');
      chart4.exporting.export('svg');
 };
