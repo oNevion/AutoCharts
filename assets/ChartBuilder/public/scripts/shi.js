37e11703600cca35504b8bc91af87fd0a1fe3a30
Jakob <jakob.bradshaw@catalystmf.com>
2021-07-29 16:58:24 -0400

    Create new SHI Chart for Flyer

--- assets/ChartBuilder/public/scripts/shi.js
+++ assets/ChartBuilder/public/scripts/shi.js
@@ -23,6 +23,11 @@ body {
 		height:650px!important;
     }
 
+    #chartdiv3 {
+        width:950px!important;
+    height:550px!important;
+    }
+
 `;
 
 // append to DOM
@@ -225,11 +230,102 @@ dateAxis.fillRule = function(dataItem) {
   }
 }
 
+// ################################   CHART 3 SHI - Flyer Covid Drop Graph ################################################
+
+
+// Themes begin
+am4core.useTheme(am4themes_amcharts);
+// Themes end
+
+// Create chart instance
+var chart3 = am4core.create("chartdiv3", am4charts.XYChart);
+
+// Set up data source
+chart3.dataSource.url = "../Data/Backups/Catalyst/SHI/SHI_FLYER_CovidDropGraph.csv";
+chart3.dataSource.parser = new am4core.CSVParser();
+chart3.dataSource.parser.options.useColumnNames = true;
+
+// Set input format for the dates
+chart3.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+chart3.numberFormatter.numberFormat = "'$'#,###.";
+
+// Create axes
+var dateAxis = chart3.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.labels.template.fontWeight = "Bold";
+dateAxis.renderer.labels.template.fontSize = "24px";
+//dateAxis.renderer.grid.template.disabled = true;
+dateAxis.renderer.labels.template.location = 0;
+//dateAxis.renderer.minGridDistance = 30;
+//dateAxis.renderer.labels.template.rotation = -45;
+dateAxis.renderer.labels.template.verticalCenter = "middle";
+dateAxis.renderer.labels.template.horizontalCenter = "middle";
+dateAxis.dateFormats.setKey("month", "MM/yyyy");
+dateAxis.periodChangeDateFormats.setKey("month", "MM/yyyy"); 
+dateAxis.renderer.maxLabelPosition = 1.3;
+//dateAxis.renderer.minLabelPosition = -0.04;
+
+
+var valueAxis = chart3.yAxes.push(new am4charts.ValueAxis());
+valueAxis.renderer.labels.template.fontWeight = "Bold";
+valueAxis.renderer.labels.template.fontSize = "24px";
+
+valueAxis.numberFormatter = new am4core.NumberFormatter();
+valueAxis.numberFormatter.numberFormat = "'$'#,###";
+
+// Create series
+var series1 = chart3.series.push(new am4charts.LineSeries());
+series1.dataFields.valueY = "Defined Shield";
+series1.dataFields.dateX = "Date";
+series1.name = "SHIIX";
+series1.strokeWidth = 3;
+series1.tooltipText = "{valueY}";
+series1.stroke = am4core.color("#4472c4");
+
+
+
+var series3 = chart3.series.push(new am4charts.LineSeries());
+series3.dataFields.valueY = "S&P";
+series3.dataFields.dateX = "Date";
+series3.name = "S&P 500 PR Index";
+series3.strokeWidth = 3;
+series3.tooltipText = "{valueY}";
+series3.stroke = am4core.color("#4d4d4f");
+
+
+
+var series4 = chart3.series.push(new am4charts.LineSeries());
+series4.dataFields.valueY = "Floor";
+series4.dataFields.dateX = "Date";
+series4.name = "Static Floor - 12.5%";
+series4.strokeWidth = 3;
+series4.tooltipText = "{valueY}";
+series4.stroke = am4core.color("#ff6a5a");
+series4.strokeDasharray = "5,5";
+
+// Add legend
+chart3.legend = new am4charts.Legend();
+chart3.legend.labels.template.fontSize = "22px";
+chart3.legend.labels.template.text = "{name}[/] [bold {color}]{valueY.open}";
+chart3.legend.labels.template.truncate = false;
+
+
+
+// Export this stuff
+chart3.exporting.menu = new am4core.ExportMenu();
+chart3.exporting.menu.items = [{
+  "label": "...",
+  "menu": [
+          { "type": "svg", "label": "SVG" },
+  ]
+}];
+chart3.exporting.filePrefix = "SHI_FLYER_CovidDrop";
+
 // ################################   Export any charts OTHER THAN chart1 ################################################
 
 function loadFrame() {
      chart1.exporting.export('svg');
      chart2.exporting.export('svg');
+     chart3.exporting.export('svg');
 };
 
 window.onload = setTimeout(loadFrame, 1800);
